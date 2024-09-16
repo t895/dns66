13630188c73ca976b1f9b4a5b6f5830a657dae43
Charles Lombardo <clombardo169@gmail.com>
2024-09-15 21:22:29 -0400

    Adjust start screen preview
    Didn't have the right theme and background set

--- app/src/main/java/org/jak_linux/dns66/ui/Start.kt
+++ app/src/main/java/org/jak_linux/dns66/ui/Start.kt
@@ -1,5 +1,6 @@
 package org.jak_linux.dns66.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.jak_linux.dns66.R
+import org.jak_linux.dns66.ui.theme.Dns66Theme
 import org.jak_linux.dns66.ui.theme.FabPadding
 import org.jak_linux.dns66.ui.theme.ListPadding
 import org.jak_linux.dns66.ui.theme.VpnFabSize
@@ -84,15 +87,18 @@ fun StartScreen(
 
 @Preview
 @Composable
-private fun StartScreenPreview(modifier: Modifier = Modifier) {
-    StartScreen(
-        resumeOnStartup = false,
-        onResumeOnStartupClick = {},
-        watchConnection = false,
-        onWatchConnectionClick = {},
-        ipv6Support = false,
-        onIpv6SupportClick = {},
-        status = VpnStatus.STOPPED,
-        onChangeVpnStatusClick = {},
-    )
+private fun StartScreenPreview() {
+    Dns66Theme {
+        StartScreen(
+            modifier = Modifier.background(MaterialTheme.colorScheme.surface),
+            resumeOnStartup = false,
+            onResumeOnStartupClick = {},
+            watchConnection = false,
+            onWatchConnectionClick = {},
+            ipv6Support = false,
+            onIpv6SupportClick = {},
+            status = VpnStatus.STOPPED,
+            onChangeVpnStatusClick = {},
+        )
+    }
 }
