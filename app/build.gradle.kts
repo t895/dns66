c954b1529af4818b249938468a906b0c510b990d
Charles Lombardo <clombardo169@gmail.com>
2024-09-18 22:00:12 -0400

    Bump version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.t895.dnsnet"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.4"
+        versionCode = 4
+        versionName = "0.1.5"
     }
 
     val storeFilePath = System.getenv("STORE_FILE_PATH")
