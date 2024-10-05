ec628454c36df20a474094fa3ac0957f89ad2f9d
Charles Lombardo <clombardo169@gmail.com>
2024-10-05 19:13:38 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,10 +76,10 @@ dependencies {
     // Proxy stuff
     implementation("org.pcap4j:pcap4j-core:1.8.2")
     implementation("org.pcap4j:pcap4j-packetfactory-static:1.8.2")
-    implementation("dnsjava:dnsjava:3.6.1")
+    implementation("dnsjava:dnsjava:3.6.2")
 
     // Compose
-    val composeBom = platform("androidx.compose:compose-bom:2024.09.01")
+    val composeBom = platform("androidx.compose:compose-bom:2024.09.03")
     implementation(composeBom)
     debugImplementation(composeBom)
     androidTestImplementation(composeBom)
@@ -94,7 +94,7 @@ dependencies {
     implementation("com.google.accompanist:accompanist-drawablepainter:$accompanistVersion")
     implementation("com.google.accompanist:accompanist-permissions:$accompanistVersion")
 
-    implementation("androidx.navigation:navigation-compose:2.8.1")
+    implementation("androidx.navigation:navigation-compose:2.8.2")
 
     val lifecycleVersion = "2.8.6"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
@@ -103,12 +103,12 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.9.2")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
     implementation("org.jetbrains.kotlinx:atomicfu:0.25.0")
 
     // Baseline profiles
-    implementation("androidx.profileinstaller:profileinstaller:1.4.0")
+    implementation("androidx.profileinstaller:profileinstaller:1.4.1")
     "baselineProfile"(project(":baselineprofile"))
 
     implementation("androidx.work:work-runtime-ktx:2.9.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("com.android.library") version "8.6.0" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
 
     val kotlinVersion = "2.0.20"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
@@ -10,9 +10,9 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
 
     id("org.jetbrains.kotlinx.atomicfu") version "0.25.0" apply false
-    id("com.android.test") version "8.6.0" apply false
+    id("com.android.test") version "8.6.1" apply false
 
-    id("androidx.baselineprofile") version "1.3.0" apply false
+    id("androidx.baselineprofile") version "1.3.2" apply false
 
     id("app.accrescent.tools.bundletool") version "0.2.4" apply false
 }
