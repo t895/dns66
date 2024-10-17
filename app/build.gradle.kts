fdd14a8f6a3c0a1b4ad0213f87fe6aa455030ce8
Charles Lombardo <clombardo169@gmail.com>
2024-10-17 14:39:06 -0400

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,14 +95,14 @@ dependencies {
 
     implementation("io.coil-kt.coil3:coil-compose:3.0.0-rc01")
 
-    implementation("androidx.navigation:navigation-compose:2.8.2")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
 
     val lifecycleVersion = "2.8.6"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
 
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.activity:activity-compose:1.9.3")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.1" apply false
-    id("com.android.library") version "8.6.1" apply false
+    val agpVersion = "8.7.1"
+    id("com.android.application") version agpVersion apply false
+    id("com.android.library") version agpVersion apply false
+    id("com.android.test") version agpVersion apply false
 
     val kotlinVersion = "2.0.20"
     id("org.jetbrains.kotlin.android") version kotlinVersion apply false
@@ -10,9 +12,8 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion apply false
 
     id("org.jetbrains.kotlinx.atomicfu") version "0.25.0" apply false
-    id("com.android.test") version "8.6.1" apply false
 
-    id("androidx.baselineprofile") version "1.3.2" apply false
+    id("androidx.baselineprofile") version "1.3.3" apply false
 
     id("app.accrescent.tools.bundletool") version "0.2.4" apply false
 }
