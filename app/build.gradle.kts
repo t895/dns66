e6f09e3d762e3f07b20bc78db4cdb595c594ca3d
Charles Lombardo <clombardo169@gmail.com>
2024-11-21 00:16:52 -0500

    Bump version to 0.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.t895.dnsnet"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.2.2"
+        versionCode = 13
+        versionName = "0.2.3"
     }
 
     val storeFilePath = System.getenv("STORE_FILE_PATH")
