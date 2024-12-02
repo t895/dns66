3d7646cb5edc3863caec769297f2e548fcbbc44c
Charles Lombardo <clombardo169@gmail.com>
2024-12-02 18:00:03 -0500

    Bump version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "dev.clombardo.dnsnet"
         minSdk = 24
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.0.2"
+        versionCode = 17
+        versionName = "1.0.3"
     }
 
     val storeFilePath = System.getenv("STORE_FILE_PATH")
